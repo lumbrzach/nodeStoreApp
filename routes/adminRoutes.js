9db602d4b1d84987a69081a0e7832033b3404555
github username <github email address>
2019-12-11 12:01:16 -0500

    added basic routing and setup

---
+++ routes/adminRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+// router.get('/', (req, res, next) => {
+//   res.status(200).send('<html><body><h1>Welcome to Express</h1></body></html>');
+// });
+
+module.exports = router;
