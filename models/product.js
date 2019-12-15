a4d326a3c82489069fbc888709c377c94c1a2fa3
github username <github email address>
2019-12-15 16:35:18 -0500

    updated products, optimized file system

--- models/product.js
+++ models/product.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const rootPath = require('../utils/path');
-const p = path.join(rootPath, 'data', 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
 const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    return cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-// This comes from 'utils' and just sets the base path consistently
-// const rootDir = require('../utils/path');
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', productsController.getAddProduct);
-router.post('/add-product', productsController.postAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-// exports everything in file as one
-// module.exports = router;
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-// breaks up file into accessible pieces when importing
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
 module.exports = router;
